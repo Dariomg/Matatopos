103a7aba3e105d4114b23781652b16f0409e44d0
dariomg <dariomg48@educastur.es>
2025-10-17 20:59:21 +0200

    Carrera

--- src/app/lista-personajes/lista-personajes.ts
+++ src/app/lista-personajes/lista-personajes.ts
@@ -10,12 +10,12 @@ import { FichaPersonaje } from '../ficha-personaje/ficha-personaje';
 })
 export class ListaPersonajes {
   personajes = [
-    { nombre: 'Godzilla', raza: 'Titanus-Gojira', poder: 5, imagen: '/Godzilla.png' },
-    { nombre: 'Ghidorah', raza: 'Titanus-Ghidorah', poder: 5, imagen: '/Ghidorah.png' },
-    { nombre: 'Mothra', raza: 'Titanus-Mosura', poder: 3, imagen: '/Mothra.png' },
-    { nombre: 'Rodan', raza: 'Titanus-Rodan', poder: 3, imagen: '/Rodan.png' },
-    { nombre: "Kong", raza: 'Titanus-Kong', poder: 4, imagen: '/Kong.png' },
-    { nombre: "Mechagodzilla", raza: 'Mecha', poder: 4, imagen: '/Mechagodzilla.png' },
-    { nombre: 'Shimo', raza: 'Titanus-Shimu', poder: 5, imagen: '/Shimo.png' }
+    { nombre: 'Godzilla', raza: 'Titanus-Gojira', poder: 5, imagen: '/imagenes-Kaijus/Godzilla.png' },
+    { nombre: 'Ghidorah', raza: 'Titanus-Ghidorah', poder: 5, imagen: '/imagenes-Kaijus/Ghidorah.png' },
+    { nombre: 'Mothra', raza: 'Titanus-Mosura', poder: 3, imagen: '/imagenes-Kaijus/Mothra.png' },
+    { nombre: 'Rodan', raza: 'Titanus-Rodan', poder: 3, imagen: '/imagenes-Kaijus/Rodan.png' },
+    { nombre: "Kong", raza: 'Titanus-Kong', poder: 4, imagen: '/imagenes-Kaijus/Kong.png' },
+    { nombre: "Mechagodzilla", raza: 'Mecha', poder: 4, imagen: '/imagenes-Kaijus/Mechagodzilla.png' },
+    { nombre: 'Shimo', raza: 'Titanus-Shimu', poder: 5, imagen: '/imagenes-Kaijus/Shimo.png' }
   ];
 }
