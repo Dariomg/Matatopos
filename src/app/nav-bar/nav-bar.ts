511f049e548721cdc707642e115b204d9f3cc226
dariomg <dariomg48@educastur.es>
2025-10-21 20:52:21 +0200

    NavBar Mejorado2

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-nav-bar',
@@ -15,20 +15,36 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     RouterLink,
     RouterOutlet,
+    MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatSlideToggleModule,
-    MatSidenavModule,
+    MatButtonModule,
     MatListModule,
-    MatButtonModule
+    MatSlideToggleModule
   ],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.css']
 })
 export class NavBar {
   modoHalloween = false;
+  isMobile = false;
+  sidenavOpened = true;
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+    this.sidenavOpened = !this.isMobile;
+  }
 
   toggleHalloween(estado: boolean) {
     this.modoHalloween = estado;
+  }
+
+  closeIfMobile(drawer: any) {
+    if (this.isMobile) drawer.close();
   }
 }
