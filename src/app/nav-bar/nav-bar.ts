cc185e351add6f480897e84723fce75fd9742a3c
dariomg <dariomg48@educastur.es>
2025-10-21 17:32:11 +0200

    NavBar Mejorado

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -2,18 +2,30 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { RouterLink } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, MatToolbarModule, MatIconModule, MatSlideToggleModule, CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
+  ],
   templateUrl: './nav-bar.html',
-  styleUrl: './nav-bar.css'
+  styleUrls: ['./nav-bar.css']
 })
 export class NavBar {
-
   modoHalloween = false;
 
   toggleHalloween(estado: boolean) {
